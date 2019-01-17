8cfe65c55747e39be23ef6c0657434f4d9d5dd23
Renan Gomes <renan.gomes@live.com>
2019-01-17 20:12:43 -0200

    GH-2 - Changing '__main__' to use flask-script to handle application/db executions.

--- server/__main__.py
+++ server/__main__.py
@@ -1,7 +1,20 @@
 from flask import Flask
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager
+
+from database.model import db
 
 app = Flask(__name__)
 app.config.from_object('dev-confguration')
 
+# Initializing database.
+db.init_app(app)
+
+migrate = Migrate(app, db)
+
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
+
+
 if __name__ == '__main__':
-    app.run()
+    manager.run()
